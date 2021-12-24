62f07fa34bdda92b0347b1e9903dc9b883a523c9
Román Ginés <rgmf@riseup.net>
2021-12-24 12:44:45 +0100

    Make chart bars generic. Cleanup. Fixes #79.

--- pyopentracks/views/graphs.py
+++ pyopentracks/views/graphs.py
@@ -23,64 +23,78 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_gtk3agg import (
     FigureCanvasGTK3Agg as FigureCanvas
 )
-from pyopentracks.utils.utils import DistanceUtils as du
-from pyopentracks.utils.utils import TypeActivityUtils as tau
 
 
-class AggregatedStatsChart:
-    def __init__(self, aggregated_lists):
-        self.aggregated_lists = aggregated_lists
-        self.figure = Figure(figsize=(1 * len(aggregated_lists), 1 * len(aggregated_lists)), dpi=100)
-        self.figure.subplots()
-        self.axes = self.figure.axes[0]
+class BarsChart:
+    def __init__(self, results, colors=None, cb_annotate=None):
+        """Creates a set of bar chart with results dictionary.
 
-        w, h = self.figure.get_size_inches()
-        dpi_res = self.figure.get_dpi()
+        Arguments:
+        results -- dictionary with values and labels for all bars to add. For example:
+                   {
+                        "Label 1": 30,
+                        "Label 2": 50,
+                        "Label 3": 40
+                   }
+        colors -- (optional) list of colors to be used for every bar.
+        cb_annotate -- (optional) it's a callback to parse the bars annotation. If it's not passed
+                       then the values will be used like are.
+        """
+        self._results = results
+        self._colors = colors
+        self._cb_annotate = cb_annotate
+
+        self._figure = Figure(figsize=(1 * len(results), 1 * len(results)), dpi=100)
+        self._figure.subplots()
+        self._axes = self._figure.axes[0]
+
+        w, h = self._figure.get_size_inches()
+        dpi_res = self._figure.get_dpi()
         w, h = int(np.ceil(w * dpi_res)), int(np.ceil(h * dpi_res))
 
-        self.figure.canvas = FigureCanvas(self.figure)
-        self.figure.canvas.set_size_request(w, h)
-        self.figure.canvas.set_has_window(False)
+        self._figure.canvas = FigureCanvas(self._figure)
+        self._figure.canvas.set_size_request(w, h)
+        self._figure.canvas.set_has_window(False)
 
     def _draw(self):
-        categories = [a.category for a in self.aggregated_lists]
-        distances = [a.total_distance_float for a in self.aggregated_lists]
+        labels = [label for label, _ in self._results.items()]
+        values = [value for _, value in self._results.items()]
 
-        self.axes.spines["left"].set_visible(False)
-        self.axes.spines["right"].set_visible(False)
-        self.axes.spines["bottom"].set_visible(False)
-        self.axes.spines["top"].set_visible(False)
-        self.axes.tick_params(left=False)
-        self.axes.tick_params(bottom=False)
-        self.axes.set_xlim([0, max(distances) + max(distances) * 0.25])
-        self.figure.patch.set_alpha(0)
-        self.axes.patch.set_alpha(0)
+        self._axes.spines["left"].set_visible(False)
+        self._axes.spines["right"].set_visible(False)
+        self._axes.spines["bottom"].set_visible(False)
+        self._axes.spines["top"].set_visible(False)
+        self._axes.tick_params(left=False)
+        self._axes.tick_params(bottom=False)
+        self._axes.set_xlim([0, max(values) + max(values) * 0.25])
+        self._figure.patch.set_alpha(0)
+        self._axes.patch.set_alpha(0)
 
-        y_pos = np.arange(len(categories))
-        rectangles = self.axes.barh(y_pos, distances)
-        self.axes.set_yticks(y_pos)
-        self.axes.set_yticklabels(categories)
+        y_pos = np.arange(len(labels))
+        rectangles = self._axes.barh(y_pos, values)
+        self._axes.set_yticks(y_pos)
+        self._axes.set_yticklabels(labels)
         # Turn off x tick labels
-        self.axes.set_xticklabels([])
+        self._axes.set_xticklabels([])
 
         # Lastly, write in the ranking inside each bar to aid in interpretation
         for i, rect in enumerate(rectangles):
             # Set rectangle color
-            rect.set_color(tau.get_color(categories[i]))
-            
+            rect.set_color(
+                self._colors[i] if self._colors is not None and len(self._colors) > i else None
+            )
+
             # Rectangle width
             width = rect.get_width()
-
             # Shift the text to the right side of the right edge
             xloc = 5
             # Black against white background
             color = "black"
             align = "left"
-
             # Center the text vertically in the bar
             yloc = rect.get_y() + rect.get_height() / 2
-            label = self.axes.annotate(
-                du.m_to_str(width * 1000),
+            self._axes.annotate(
+                self._cb_annotate(width) if self._cb_annotate is not None else width,
                 xy=(width, yloc),
                 xytext=(xloc, 0),
                 textcoords="offset points",
@@ -92,12 +106,12 @@ class AggregatedStatsChart:
             )
 
     def get_canvas(self):
-        return self.figure.canvas
+        return self._figure.canvas
 
     def draw_and_show(self):
         self._draw()
-        self.figure.canvas.draw()
-        self.figure.canvas.show()
+        self._figure.canvas.draw()
+        self._figure.canvas.show()
 
 
 class StackedBarsChart:
@@ -117,51 +131,53 @@ class StackedBarsChart:
         cb_annotate -- (optional) it's a callback to be called for set the annotation on bar chart.
         """
 
-        self.cb_annotate = cb_annotate
+        self._cb_annotate = cb_annotate
 
-        self.figure = Figure(figsize=(1, 1), dpi=100)
-        self.figure.subplots()
-        self.axes = self.figure.axes[0]
+        self._accum = 0
+        self._bars = []
+        self._annotate = None
 
-        self.axes.spines["left"].set_visible(False)
-        self.axes.spines["right"].set_visible(False)
-        self.axes.spines["bottom"].set_visible(False)
-        self.axes.spines["top"].set_visible(False)
-        self.axes.tick_params(left=False)
-        self.axes.tick_params(bottom=False)
+        self._figure = Figure(figsize=(1, 1), dpi=100)
+        self._figure.subplots()
+        self._axes = self._figure.axes[0]
 
-        w, h = self.figure.get_size_inches()
-        dpi_res = self.figure.get_dpi()
+        self._axes.spines["left"].set_visible(False)
+        self._axes.spines["right"].set_visible(False)
+        self._axes.spines["bottom"].set_visible(False)
+        self._axes.spines["top"].set_visible(False)
+        self._axes.tick_params(left=False)
+        self._axes.tick_params(bottom=False)
+
+        w, h = self._figure.get_size_inches()
+        dpi_res = self._figure.get_dpi()
         w, h = int(np.ceil(w * dpi_res)), int(np.ceil(h * dpi_res))
 
-        self.figure.canvas = FigureCanvas(self.figure)
-        self.figure.canvas.set_size_request(w, h)
+        self._figure.canvas = FigureCanvas(self._figure)
+        self._figure.canvas.set_size_request(w, h)
 
         labels = list(results.keys())
         data = np.array(list(results.values()))
         data_cum = data.cumsum(axis=1)
 
-        self.axes.invert_yaxis()
-        self.axes.xaxis.set_visible(False)
+        self._axes.invert_yaxis()
+        self._axes.xaxis.set_visible(False)
         max_x = max_width if max_width else np.sum(data, axis=1).max()
-        self.axes.set_xlim([0, max_x + max_x * 0.25])
+        self._axes.set_xlim([0, max_x + max_x * 0.25])
 
         # Adds stacked bars if data
         if data is None or len(data) == 0 or len(data[0]) == 0:
             return
-        self.accum = 0
         rects = None
-        self.bars = []
         for i in range(len(data[0])):
             widths = data[:, i]
             starts = data_cum[:, i] - widths
             color = colors[i] if colors is not None and len(colors) > i else None
-            rects = self.axes.barh(labels, widths, left=starts, height=0.75, color=color)
+            rects = self._axes.barh(labels, widths, left=starts, height=0.75, color=color)
             # Annotate the stacked bars in the middle of the bar.
             # for bar in rects:
             #     width = bar.get_width()
             #     yloc = bar.get_y() + bar.get_height() / 2
-            #     self.axes.annotate(
+            #     self._axes.annotate(
             #             cb_annotate(width) if cb_annotate else width,
             #             xy=(bar.get_x(), yloc),
             #             xytext=(5, 0),
@@ -173,15 +189,15 @@ class StackedBarsChart:
             #             clip_on=True
             #         )
             for bar in rects:
-                self.bars.append(bar)
-                self.accum += bar.get_width()
+                self._bars.append(bar)
+                self._accum += bar.get_width()
 
         # Annotate the stacked bars at the end (out from last bar)
         xloc = 5
         yloc = rects[-1].get_y() + rects[-1].get_height() / 2
-        self.annotate = self.axes.annotate(
-            cb_annotate(self.accum) if cb_annotate else str(self.accum),
-            xy=(self.accum, yloc),
+        self._annotate = self._axes.annotate(
+            cb_annotate(self._accum) if cb_annotate else str(self._accum),
+            xy=(self._accum, yloc),
             xytext=(xloc, 0),
             textcoords="offset points",
             horizontalalignment="left",
@@ -191,45 +207,46 @@ class StackedBarsChart:
             clip_on=True
         )
 
-        self.figure.canvas.mpl_connect("motion_notify_event", lambda event: self._on_mouse_hover(event))
+        self._figure.canvas.mpl_connect(
+            "motion_notify_event",
+            lambda event: self._on_mouse_hover(event)
+        )
 
     def _update_annotate(self, bar):
+        if self._annotate is None:
+            return
+
         x = bar.get_x() + bar.get_width() / 2
         y = bar.get_y() + bar.get_height()
-        #annot.xy = (x,y)
-        text = f"{x}"
-        self.annotate.set_text(self.cb_annotate(bar.get_width() if self.cb_annotate else str(bar.get_width())))
-        self.annotate.set_color(bar.get_facecolor())
-        self.figure.canvas.draw_idle()
-        #annot.get_bbox_patch().set_alpha(0.4)
+        self._annotate.set_text(
+            self._cb_annotate(bar.get_width() if self._cb_annotate else str(bar.get_width()))
+        )
+        self._annotate.set_color(bar.get_facecolor())
+        self._figure.canvas.draw_idle()
 
     def _on_mouse_hover(self, event):
-        #vis = annot.get_visible()
-        if event.inaxes == self.axes:
-            for bar in self.bars:
+        if self._annotate is None:
+            return
+
+        if event.inaxes == self._axes:
+            for bar in self._bars:
                 bar_contains_event, details = bar.contains(event)
                 if bar_contains_event:
                     self._update_annotate(bar)
-                    #annot.set_visible(True)
-                    #self.figure.canvas.draw_idle()
-                    #accum_annotate.set_visible(False)
-                    #self.figure.canvas.draw_idle()
                     return
-                # else:
-                #     accum_annotate.set_visible(True)
-                #     self.figure.canvas.draw_idle()
-        # if vis:
-        #accum_annotate.set_visible(True)
-        self.annotate.set_text(self.cb_annotate(self.accum) if self.cb_annotate else str(self.accum))
-        self.annotate.set_color("black")
-        self.figure.canvas.draw_idle()
+
+        self._annotate.set_text(
+            self._cb_annotate(self._accum) if self._cb_annotate else str(self._accum)
+        )
+        self._annotate.set_color("black")
+        self._figure.canvas.draw_idle()
 
     def get_canvas(self):
-        return self.figure.canvas
+        return self._figure.canvas
 
     def draw_and_show(self):
-        self.figure.canvas.draw()
-        self.figure.canvas.show()
+        self._figure.canvas.draw()
+        self._figure.canvas.show()
 
 
 class LinePlot:
@@ -256,13 +273,13 @@ class LinePlot:
         self._yvalues = [ item['elevation'] for item in values ]
         self._locations = [ item['location'] for item in values ]
 
-        self.axes = self._figure.axes[0]
-        self.axes.spines["left"].set_visible(True)
-        self.axes.spines["right"].set_visible(False)
-        self.axes.spines["bottom"].set_visible(True)
-        self.axes.spines["top"].set_visible(False)
-        #self.axes.plot([0, 200],[50, 50],'--g',label='min: '+str(50)+' m')
-        self.axes.fill_between(self._xvalues, 0, self._yvalues, facecolor="green", alpha=0.5)
+        self._axes = self._figure.axes[0]
+        self._axes.spines["left"].set_visible(True)
+        self._axes.spines["right"].set_visible(False)
+        self._axes.spines["bottom"].set_visible(True)
+        self._axes.spines["top"].set_visible(False)
+        #self._axes.plot([0, 200],[50, 50],'--g',label='min: '+str(50)+' m')
+        self._axes.fill_between(self._xvalues, 0, self._yvalues, facecolor="green", alpha=0.5)
 
     def get_canvas(self):
         return self._canvas
