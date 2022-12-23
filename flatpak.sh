fc4abadc86ba59b4ff4bd7f5cc2d77f2d8a6cf62
Román Martínez <rgmf@riseup.net>
2022-12-23 13:58:23 +0100

    Prepared the project to build/test through Flatpak.

--- flatpak.sh
+++ flatpak.sh
@@ -1,2 +1,5 @@
+rm -rf .pyopentracks-dev 2> /dev/null
+mkdir .pyopentracks-dev
+cp -r build-aux data po pyopentracks tests es.rgmf.pyopentracks-dev.json meson.build .pyopentracks-dev/.
 flatpak-builder --force-clean --user --install flatpakdir es.rgmf.pyopentracks-dev.json
 flatpak run --socket=session-bus es.rgmf.pyopentracks-dev
